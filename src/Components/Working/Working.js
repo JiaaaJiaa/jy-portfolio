211cf91265cdfce46cde56033956a0c4bd5ca454
JiaaaJiaa <jiaying01@student.usm.my>
2024-12-14 00:14:44 +0800

    Update Working Feature

--- src/Components/Working/Working.js
+++ src/Components/Working/Working.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import './Working.css';
+import titlebackground from '../../assets/titlebackground.png';
 
 const Working = () => {
     return ( 
         <div className="work">
-            <h1>Working Experience</h1>
+            <div className="work-title">
+                <h1>Working Experience</h1>
+                <img src={titlebackground} alt="" />
+            </div>
+
             <div className="work-content">
                 <div className="work-item">
-                    <h2>Internship at Fave</h2>
-                    <p>Software Engineer Intern</p>
-                    <p>Jun, 2023 - Aug, 2023</p>
+                    <h2>ASE Electronics (M) Sdn Bhd</h2>
+                    <p>Internship</p>
+                    <p>Mar, 2023 - Sept, 2023 </p>
                     <div className="work-desc">
                         <ul>
-                            <li>Developed and maintained web applications using ReactJS</li>
-                            <li>Worked on the frontend and backend of the application</li>
-                            <li>Participated in code reviews and testing</li>
+                            <li>Involved in Python-based Tesseract OCR projects. </li>
+                            <li>Utilized Arduino and MQTT protocol with open-source tools.</li>
+                            <li>Explored diverse Machine Learning/Deep Learning tools and learning to train models that address real-world problems</li>
                         </ul>
                     </div>
                 </div>
                 <div className="work-item">
-                    <h2>Internship at Intel</h2>
-                    <p>Software Engineer Intern</p>
-                    <p>Jun, 2022 - Aug, 2022</p>
+                    <h2>Tadika Seri Cahaya</h2>
+                    <p>Assistant Teacher</p>
+                    <p>Jan, 2019 - Mar, 2019</p>
                     <div className="work-desc">
                         <ul>
-                            <li>Developed and maintained web applications using ReactJS</li>
-                            <li>Worked on the frontend and backend of the application</li>
-                            <li>Participated in code reviews and testing</li>
+                            <li>Assisted 60 children from 4 – 6 years old</li>
                         </ul>
                     </div>
                 </div>
